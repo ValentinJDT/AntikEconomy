c0bb97c2091665d2d11c7cf2885c5e8aa814e6a5
ValentinJDT <vjeandot@yahoo.com>
2023-07-24 18:27:48 +0200

    rename antikbase to instance

--- src/main/java/antikeconomy/economy/VaultHook.java
+++ src/main/java/antikeconomy/economy/VaultHook.java
@@ -8,18 +8,18 @@ import org.bukkit.plugin.ServicePriority;
 
 public class VaultHook {
 
-    private AntikEconomy antikBase = AntikEconomy.getInstance();
+    private AntikEconomy instance = AntikEconomy.getInstance();
 
     private Economy provider;
 
     public void hook() {
-        provider = antikBase.economyImplementer;
-        Bukkit.getServicesManager().register(Economy.class, this.provider, this.antikBase, ServicePriority.Normal);
-        Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "VaultAPI hooked into " + ChatColor.AQUA + antikBase.getName());
+        provider = instance.economyImplementer;
+        Bukkit.getServicesManager().register(Economy.class, this.provider, this.instance, ServicePriority.Normal);
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "VaultAPI hooked into " + ChatColor.AQUA + instance.getName());
     }
 
     public void unhook() {
         Bukkit.getServicesManager().unregister(Economy.class, this.provider);
-        Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "VaultAPI unhooked from " + ChatColor.AQUA + antikBase.getName());
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "VaultAPI unhooked from " + ChatColor.AQUA + instance.getName());
     }
 }
